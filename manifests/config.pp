4f018a79ad4c0f5ec4f171d0a6f8917378c9301b
Anton Lindström <lindztr@gmail.com>
2014-02-28 09:07:05 +0000

    add server.xml config

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,7 +2,9 @@
 #
 # This class is called from jira
 #
-class jira::config {
+class jira::config(
+  $secure_proxy = $jira::params::secure_proxy
+) {
   include jira::params
 
   $jira_home    = $jira::params::home
@@ -26,6 +28,15 @@ class jira::config {
     owner  => root,
     group  => root,
     source => 'puppet:///modules/jira/setenv.sh',
+  }
+
+  file { 'server.xml':
+    ensure  => present,
+    path    => "/opt/atlassian-jira-${jira_version}-standalone/conf/server.xml",
+    mode    => '0755',
+    owner   => root,
+    group   => root,
+    content => template('jira/server.xml.erb'),
   }
 
 }
