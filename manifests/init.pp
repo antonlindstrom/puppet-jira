49f93507266caac75ebf7522584fd1fba6836c58
Anton Lindström <lindztr@gmail.com>
2014-03-06 12:35:17 +0000

    add version as init parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,7 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class jira (
+  $version        = $jira::params::version,
   $minimum_memory = $jira::params::minimum_memory,
   $maximum_memory = $jira::params::maximum_memory,
   $permgen_size   = $jira::params::permgen_size,
